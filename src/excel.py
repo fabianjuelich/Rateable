6afc25a217062239cae39ce641964106328fc087
Fabian <63878623+fabianjuelich@users.noreply.github.com>
2023-05-26 13:10:24 +0200

    Add hyperlinks to table

--- src/excel.py
+++ src/excel.py
@@ -4,4 +4,9 @@ class Excel():
     def create(self, table, con, path):
         df = df = pd.read_sql_query(f'SELECT * FROM {table}', con)
         df.columns = ['Name', 'Rating stars', 'Number of ratings', 'Source', 'Path']
+        df['Source'] = df['Source'].apply(self.hyperlink, args=('Audible',))
+        df['Path'] = df['Path'].apply(self.hyperlink, args=('Explorer',))
         df.to_excel(path, index=False)
+
+    def hyperlink(self, link, name):
+        return f'=HYPERLINK("{link}", "{name}")' if link != 'N/A' else ''
