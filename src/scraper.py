f23f023a73a6330f79c3be7a32b99cdb49d2f679
Fabian <63878623+fabianjuelich@users.noreply.github.com>
2023-05-26 13:09:47 +0200

    Shorten url and handle exception

--- src/scraper.py
+++ src/scraper.py
@@ -36,7 +36,8 @@ class Scraper():
         # rating
         stars = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[6]/span[2]').text
         text = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[6]').text.split('\n')[1]
-        return stars, text, self.driver.current_url
+        url = self.driver.find_element(By.XPATH, '/html/head/meta[11]').get_attribute('content')
+        return stars, text, url
 
     def __convert_rating(self, stars):
         # ToDo: consider edgecases 0 and 5
@@ -56,6 +57,9 @@ class Scraper():
         return int(number.replace('.', ''))
 
     def get_rating(self, keyword, byte=False):
-        stars, text, url = self.__scrape_rating(keyword)
-        stars = float(stars.replace(',', '.'))
-        return self.__convert_rating(stars) if byte else stars, self.__format_number(text), url
+        try:
+            stars, text, url = self.__scrape_rating(keyword)
+            stars = float(stars.replace(',', '.'))
+            return self.__convert_rating(stars) if byte else stars, self.__format_number(text), url
+        except:
+            return 'NULL', 'NULL', 'N/A'
